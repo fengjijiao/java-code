56ba616df8764f0b1fd6984d6182d41bfe8240ba
JiJiao Feng <16521693+fengjijiao@users.noreply.github.com>
2020-04-15 19:40:40 +0800

    Create 多线程不同步问题-解决方法1.java

---
+++ 多线程不同步问题-解决方法1.java
@@ -0,0 +1,43 @@
+package test2;
+
+public class test11 {
+    public static void main(String[] args) {
+        Account account = new Account();
+        User user = new User(account, 3000);
+        Thread thread = new Thread(user);
+        User user1 = new User(account, 3000);
+        Thread thread1 = new Thread(user1);
+        thread.start();
+        thread1.start();
+        /*
+        *余额:2000
+        *余额不足！
+        *Thread-0已消费:3000
+        *Thread-1已消费:3000
+        */
+    }
+}
+class User implements Runnable {
+    Account account;
+    int m;
+    User(Account account, int m) {
+        this.account = account;
+        this.m = m;
+    }
+    @Override
+    public void run() {
+        account.consume(m);
+        System.out.println(Thread.currentThread().getName() + "已消费:" + m);
+    }
+}
+class Account {
+    int money = 5000;
+    synchronized void consume(int m) {//直接在执行方法上加上synchronized
+        if(money - m < 0) {
+            System.out.println("余额不足！");
+            return;
+        }
+        money -= m;
+        System.out.println("余额:" + money);
+    }
+}
