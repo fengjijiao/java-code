7ed82f1f6d4fb58d76d1939c9c36ee899ec6a3d6
JiJiao Feng <16521693+fengjijiao@users.noreply.github.com>
2020-04-16 08:08:41 +0800

    Update 多线程不同步问题-解决方法3.java

--- 多线程不同步问题-解决方法3.java
+++ 多线程不同步问题-解决方法3.java
@@ -5,20 +5,20 @@ public class test11 {
         Account account = new Account();
         Account account1 = new Account();
         User user = new User(account, 3000);
-        Thread thread = new Thread(user);
-        User user1 = new User(account1, 3000);
-        Thread thread1 = new Thread(user1);
+        Thread thread = new Thread(user, "客户端1");
+        User user1 = new User(account1, 4000);
+        Thread thread1 = new Thread(user1, "客户端2");
         thread.start();
         thread1.start();
         /*
-        *余额:2000
-        *余额不足！
-        *Thread-0已消费:3000
-        *Thread-1已消费:3000
+        *客户端1已消费:3000
+        *客户端1余额:2000
+         *客户端2余额不足！
         */
     }
 }
 class User implements Runnable {
+    static Thread thread;
     Account account;
     int m;
     User(Account account, int m) {
@@ -27,18 +27,35 @@ class User implements Runnable {
     }
     @Override
     public void run() {
-        account.consume(m);
-        System.out.println(Thread.currentThread().getName() + "已消费:" + m);
+        /**这种方式必须保证synchronized作用对象相同例如，都作用于thread
+         */
+        if(Thread.currentThread().getName().equals("客户端1")) {
+            thread = Thread.currentThread();
+            synchronized (thread) {
+                try {
+                    thread.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        account.consume(Thread.currentThread().getName(), m);
+        if(Thread.currentThread().getName().equals("客户端2")) {
+            synchronized (thread) {
+                thread.notify();
+            }
+        }
     }
 }
 class Account {
-    static int money = 5000;
-    void consume(int m) {
+    static int money = 5000;//注意这里必须是类变量
+    void consume(String name, int m) {
         if(money - m < 0) {
-            System.out.println("余额不足！");
+            System.out.println(name + "余额不足！");
             return;
         }
+        System.out.println(name + "已消费:" + m);
         money -= m;
-        System.out.println("余额:" + money);
+        System.out.println(name + "余额:" + money);
     }
 }
